8b5bae73a613edfc5faf6632db6049e6408334be
Jan <jrejasii@gmail.com>
2022-05-13 18:49:31 -0500

    fixed issue with pointer

---
+++ java.js
@@ -0,0 +1,15 @@
+const pinOne_div = document.getElementById("pinOne");
+
+pinOne_div.onclick = function(){
+    console.log("Hello");
+}
+
+// function main(){
+
+//     pinOne_div.addEventListener('click', () =>{
+//         console.log("hello");
+//     })
+  
+// }
+
+// main();
