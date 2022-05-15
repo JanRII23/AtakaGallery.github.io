43b124367931ed2b8604c53ec0540ebe0a30bb5e
Jan <jrejasii@gmail.com>
2022-05-15 08:04:40 -0500

    changed features on portrait, landscape, and doing only three pins

--- java.js
+++ java.js
@@ -1,12 +1,29 @@
-const pinOne_div = document.getElementById("pinOne");
+// const pinOne_div = document.getElementById("pinOne");
+const pinOne_div = document.querySelector(".pinOne");
+
+const pinTwo_div = document.querySelector(".pinTwo");
+const pinThree_div = document.querySelector(".pinThree");
 
 
 function main(){
 
    pinOne_div.addEventListener('click', () => {
        console.log("Hello");
-       pinOne_div.classList.toggle('visited');
+       pinOne_div.classList.toggle('active');
    })
+
+
+   pinTwo_div.addEventListener('click', () => {
+        console.log("Hello");
+        pinTwo_div.classList.toggle('active');
+    })
+ 
+
+    pinThree_div.addEventListener('click', () => {
+        console.log("Hello");
+        pinThree_div.classList.toggle('active');
+    })
+ 
     
 
 }
