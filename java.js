79a15cfd51472c9f80eb00821c248557b41c029b
Jan <jrejasii@gmail.com>
2022-05-16 14:15:38 -0500

    fixed the night mode on mobile and changed the scaling of the pins whenever on mobile

--- java.js
+++ java.js
@@ -10,6 +10,7 @@ const exploreLetters = document.querySelector(".explore");
 const menuLetters = document.querySelector(".menu");
 const contactButton = document.querySelector(".contact");
 const logoNight = document.querySelector(".menu-toggle");
+
 const nightMode_li = document.getElementById("night");
 
 
@@ -26,16 +27,18 @@ function main(){
  
 
     pinThree_div.addEventListener('click', () => {
+        console.log("Hello");
         pinThree_div.classList.toggle('active');
     })
 
     nightMode_li.addEventListener('click', () =>{
         nightMode_div.classList.toggle('active');
         nightModeLetters.classList.toggle('active');
-        menuLetters.classList.toggle('active');
-        exploreLetters.classList.toggle('active');
         contactButton.classList.toggle('active');
         logoNight.classList.toggle('active');
+        menuLetters.classList.toggle('active');
+        exploreLetters.classList.toggle('active');
+
     })
 
 
