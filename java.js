411b1c0cc969ab8c5776ed45dd1c88dbdf55f1e2
Jan <jrejasii@gmail.com>
2022-05-15 20:51:29 -0500

    changes on the main background as a url instead of an img (html)

--- java.js
+++ java.js
@@ -1,6 +1,5 @@
 // const pinOne_div = document.getElementById("pinOne");
 const pinOne_div = document.querySelector(".pinOne");
-
 const pinTwo_div = document.querySelector(".pinTwo");
 const pinThree_div = document.querySelector(".pinThree");
 
@@ -8,23 +7,20 @@ const pinThree_div = document.querySelector(".pinThree");
 function main(){
 
    pinOne_div.addEventListener('click', () => {
-       console.log("Hello");
        pinOne_div.classList.toggle('active');
    })
 
 
    pinTwo_div.addEventListener('click', () => {
-        console.log("Hello");
         pinTwo_div.classList.toggle('active');
     })
  
 
     pinThree_div.addEventListener('click', () => {
-        console.log("Hello");
         pinThree_div.classList.toggle('active');
     })
- 
-    
+
+
 
 }
 
