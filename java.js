a4143cfa1ff4781652589b99a5d872d270452e02
Jan <jrejasii@gmail.com>
2022-06-09 20:30:43 -0500

    almost done with the moving spotlight during dark mode

--- java.js
+++ java.js
@@ -12,8 +12,26 @@ const menuLetters = document.querySelector(".menu");
 const contactButton = document.querySelector(".contact");
 const logoNight = document.querySelector(".menu-toggle");
 
+let spotLight_div = document.querySelector(".spotlight");
+
 const nightMode_li = document.getElementById("night");
 
+
+function moveSpotlight(){ //check to see if spotlights goes away when switching off night also what is happening is that im querying .spotlight not spotlight active"
+    
+    let spotLightActive = document.querySelector(".spotlight.active");
+
+    window.addEventListener('mousemove', function(e){
+        let percentageX = e.pageX / window.innerWidth * 100;
+        let percentageY = e.pageY / window.innerHeight * 100;
+
+        spotLightActive.style.backgroundIMage = `radial-gradient(circle at ${percentageX}% ${percentageY}%, transparent 80px, rgba(0,0,0,0.45) 140px`
+        
+    })
+    console.log("Hello");
+
+    
+}
 
 function main(){
 
@@ -39,12 +57,14 @@ function main(){
         logoNight.classList.toggle('active');
         menuLetters.classList.toggle('active');
         exploreLetters.classList.toggle('active');
-
+        spotLight_div.classList.toggle('active');
     })
 
-
+ 
 
 }
 
 main();
+
+moveSpotlight();
 
