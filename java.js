1b3dfbb5d75f7667b77a8b54eda0a1707c24befa
Jan <jrejasii@gmail.com>
2022-07-21 14:50:42 -0500

    made some more changes in the hierarchy structure and jquery

--- java.js
+++ java.js
@@ -1,75 +1,62 @@
-// const pinOne_div = document.getElementById("pinOne");
-const pinOne_div = document.querySelector(".pinOne");
-const pinTwo_div = document.querySelector(".pinTwo");
-const pinThree_div = document.querySelector(".pinThree");
+const nightMode_SpotLightModule = (function(){
+    'use strict';
+    const nightMode_div = document.querySelector(".backgroundPic");
+    const nightModeLetters = document.querySelector(".flex-container");
+    const exploreLetters = document.querySelector(".explore");
+    const menuLetters = document.querySelector(".menu");
+    const contactButton = document.querySelector(".contact");
+    const logoNight = document.querySelector(".menu-toggle");
 
-const nightMode_div = document.querySelector(".backgroundPic");
-const nightModeLetters = document.querySelector(".flex-container");
-const exploreLetters = document.querySelector(".explore");
-const menuLetters = document.querySelector(".menu");
-const contactButton = document.querySelector(".contact");
-const logoNight = document.querySelector(".menu-toggle");
+    let spotLight_div = document.querySelector(".spotlight");
+    let click = false;
 
-let spotLight_div = document.querySelector(".spotlight");
-let click = false;
+    const nightMode_li = document.getElementById("night");
 
-const nightMode_li = document.getElementById("night");
-
-
-function moveSpotlight(){ 
+    function moveSpotlight(){ 
     
-    window.addEventListener('mousemove', e => {
-        let percentageX = e.pageX / window.innerWidth * 100;
-        let percentageY = e.pageY / window.innerHeight * 100;
+        window.addEventListener('mousemove', e => {
+            let percentageX = e.pageX / window.innerWidth * 100;
+            let percentageY = e.pageY / window.innerHeight * 100;
+    
+            if(click == true){
+            spotLight_div.style.backgroundImage = `radial-gradient(circle at ${percentageX}% ${percentageY}%, transparent 80px, rgba(0,0,0,0.20) 110px`
+            }
+    
+        })
+    };
 
-        if(click == true){
-        spotLight_div.style.backgroundImage = `radial-gradient(circle at ${percentageX}% ${percentageY}%, transparent 80px, rgba(0,0,0,0.20) 110px`
+    function nightMode(){
+
+        nightMode_li.addEventListener('click', () =>{
+            nightMode_div.classList.toggle('active');
+            nightModeLetters.classList.toggle('active');
+            contactButton.classList.toggle('active');
+            logoNight.classList.toggle('active');
+            menuLetters.classList.toggle('active');
+            exploreLetters.classList.toggle('active');
+            spotLight_div.classList.toggle('active');
+            
+        })
+    };
+
+    return{
+        moveSpotlightPublic: function(){
+            moveSpotlight();
+        },
+        nightModePublic: function(){
+            nightMode();
         }
-
-    })
-
+    };
     
-}
-
-function nightMode(){
-
-    nightMode_li.addEventListener('click', () =>{
-        nightMode_div.classList.toggle('active');
-        nightModeLetters.classList.toggle('active');
-        contactButton.classList.toggle('active');
-        logoNight.classList.toggle('active');
-        menuLetters.classList.toggle('active');
-        exploreLetters.classList.toggle('active');
-        spotLight_div.classList.toggle('active');
-        
-    })
-}
-
-
+}) ();
 
 function main(){
 
-//    pinOne_div.addEventListener('click', () => {
-//        pinOne_div.classList.toggle('active');
-       
-//    })
-
-
-//    pinTwo_div.addEventListener('click', () => {
-//         pinTwo_div.classList.toggle('active');
-//     })
- 
-
-//     pinThree_div.addEventListener('click', () => {
-//         pinThree_div.classList.toggle('active');
-//     })
-
-    nightMode();
-
-    moveSpotlight();
+    nightMode_SpotLightModule.nightModePublic();
+    nightMode_SpotLightModule.moveSpotlightPublic();
 
     document.querySelector("body").addEventListener('click', ()=>{
-        click = !click;
+        click = !click; //this is a neat trick
     })
 
 
@@ -78,7 +65,18 @@ function main(){
 main();
 
 
+//for the pins later
 
-//new implementation is to have the spotlight stop when clicking the body using the click = !click trick if user ever decides not to have the spotlight stay
+// const pinOne_div = document.querySelector(".pinOne");
+// const pinTwo_div = document.querySelector(".pinTwo");
+// const pinThree_div = document.querySelector(".pinThree");
 
-//need to make the js modular and encapsulated
+//    pinOne_div.addEventListener('click', () => {
+//        pinOne_div.classList.toggle('active');
+//    })
+//    pinTwo_div.addEventListener('click', () => {
+//         pinTwo_div.classList.toggle('active');
+//     })
+//     pinThree_div.addEventListener('click', () => {
+//         pinThree_div.classList.toggle('active');
+//     })
