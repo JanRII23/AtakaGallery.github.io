a73ad9dfed669255c3f93885079bcdb4cbdfaf1b
Jan <jrejasii@gmail.com>
2022-07-30 12:16:53 -0500

    almost finished with the pin and arrow animations

--- java.js
+++ java.js
@@ -57,6 +57,33 @@
 
 let click = false;
 
+const unityPage_Module = (function(){
+    'use strict';
+
+    const galleryyPint_real_img = document.querySelector('.galleryPin_real');
+    const arrowContainer1_div = document.querySelector('.arrowContainer1')
+    const arrowContainer2_div = document.querySelector('.arrowContainer2')
+    const arrowContainer3_div = document.querySelector('.arrowContainer3')
+    const arrowContainer4_div = document.querySelector('.arrowContainer4')
+    const arrowContainer5_div = document.querySelector('.arrow5')
+
+    function pinClick(){
+        galleryyPint_real_img.addEventListener('click', () => {
+            galleryyPint_real_img.classList.toggle('active');
+            arrowContainer1_div.classList.toggle('active');
+            arrowContainer2_div.classList.toggle('active');
+            arrowContainer3_div.classList.toggle('active');
+            arrowContainer4_div.classList.toggle('active');
+            arrowContainer5_div.classList.toggle('active');
+        })
+    };
+
+    return {
+        pinClickPublic: function(){
+            pinClick();
+        }
+    }
+}) ();
 
 const removeMp4_Module = (function(){
 
@@ -102,6 +129,7 @@ const unityPlay_Module = (function(){
     const flexContainer2First_div = document.querySelector(".flex-container2First");
     const flexContainer2Second_div = document.querySelector(".flex-container2Second");
     const flex_container2_id = document.querySelector(".flex-container2");
+    const galleryInfo_div = document.querySelector('.galleryInfo');
     
 
 
@@ -113,6 +141,7 @@ const unityPlay_Module = (function(){
             flexContainer2First_div.classList.toggle('active');
             flexContainer2Second_div.classList.toggle('active');
             flex_container2_id.classList.toggle('ease');
+            galleryInfo_div.classList.toggle('active');
             // flexContainer2_id.classList.toggle('easeBackground');
         })
 
@@ -192,8 +221,9 @@ function main(){
 
     nightMode_SpotLightModule.nightModePublic();
     nightMode_SpotLightModule.moveSpotlightPublic();
-    // unityPlay_Module.stopVideoPublic(); 
-    // removeMp4_Module.LoadPublic();
+    unityPlay_Module.stopVideoPublic(); 
+    removeMp4_Module.LoadPublic();
+    unityPage_Module.pinClickPublic();
 
     document.querySelector("body").addEventListener('click', ()=>{
         click = !click; //this is a neat trick
